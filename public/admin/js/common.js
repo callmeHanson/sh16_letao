e3ee9c31ee6898ba45cd51848ffc816f9174c1d8
wuhang <callmeHanson>
2017-11-29 18:49:21 +0800

    index页面left的aside完成布局

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -1,1 +1,23 @@
+$(function () {
 
+// 关于进度条
+
+// 配置进度条//取消右侧的进度环
+NProgress.configure({	showSpinner: false});
+
+
+// 1. ajax发送开始
+$(document).ajaxStart(function () {
+	NProgress.start();
+})
+
+// 2.ajax发送结束
+$(document).ajaxStop(function () {
+	// ajax请求发送完成的时间太快，看不出效果
+	// 需要添加一个定时器（setTimeout）来延长时间
+	setTimeout(function () {
+		NProgress.done();
+	}, 500);
+})
+
+})
